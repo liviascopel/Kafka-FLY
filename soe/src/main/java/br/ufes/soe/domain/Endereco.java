16e954bc1b355309660e20222059ff16e39bd5d2
thaisssssssssss <thaisgomes1585@gmail.com>
2026-06-29 11:14:45 -0300

    feat: calculo das coordenadas do usuario

--- soe/src/main/java/br/ufes/soe/domain/Endereco.java
+++ soe/src/main/java/br/ufes/soe/domain/Endereco.java
@@ -4,19 +4,41 @@ import jakarta.persistence.Embeddable;
 
 @Embeddable
 public class Endereco {
+    private String estado;
     private String cidade;
     private String bairro;
     private String rua;
     private int numero;
 
-    public Endereco(String cidade, String bairro, String rua, int numero){
+    private Double latitude;
+    private Double longitude;
+    
+    public Endereco(String estado, String cidade, String bairro, String rua, int numero){
+        setEstado(estado);
         setCidade(cidade);
         setBairro(bairro);
         setRua(rua);
         setNumero(numero);
     }
-
+    
     protected Endereco(){}
+    
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
     
     public int getNumero() {
         return numero;
@@ -48,5 +70,13 @@ public class Endereco {
 
     public void setBairro(String bairro) {
         this.bairro = bairro;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
     }
 }
